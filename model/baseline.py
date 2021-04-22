5aab95cb48d66991ae2c9e9cc5ba5a0bee80ba6c
daibingh <daibingh@gmail.com>
2021-04-22 17:56:57 +0800

    delete unused file, adjust model dir

--- model/baseline.py
+++ model/baseline.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import numpy as np
 import pandas as pd 
 from utils.utils import *
-from utils.transformer import TransformerEncoderLayer as TFE
+from .transformer import TransformerEncoderLayer as TFE
 
 
 class MaskLSTM(nn.Module):
