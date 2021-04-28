4ef16df263875329a6c5f36cea8beb49c3424666
daibingh <daibingh@gmail.com>
2021-04-29 07:48:27 +0800

    add swa_utils

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
 sudo docker container run -it registry.cn-shenzhen.aliyuncs.com/sduhdb/mednlp:$1 /bin/bash
-sh rm_container.sh
+#sh rm_container.sh
--- train.py
+++ train.py
@@ -306,8 +306,8 @@ if __name__ == "__main__":
             if F.cos_lr:
                 base_opt = torch.optim.AdamW(lr=F.lr, params=model.parameters(), weight_decay=F.weight_decay)
                 # lookahead = Lookahead(base_opt, k=5, alpha=0.5)
-                lr_scheduler = LambdaLR(base_opt, lr_lambda=lambda epoch:  cosine_lr(epoch, max_epoch=F.epochs, offset=5) )
-                # lr_scheduler = CosineAnnealingWarmRestarts(base_opt, T_0=F.T_0, T_mult=1)
+                # lr_scheduler = LambdaLR(base_opt, lr_lambda=lambda epoch:  cosine_lr(epoch, max_epoch=F.epochs, offset=5) )
+                lr_scheduler = CosineAnnealingWarmRestarts(base_opt, T_0=F.T_0, T_mult=1)
 
             T.train(F, 
                     model, 
