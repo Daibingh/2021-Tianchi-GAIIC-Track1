00255a29b735dd3a3e04f5ea1daaf7ac6dc138c3
daibingh <daibingh@gmail.com>
2021-04-29 18:47:49 +0800

    chagne config for swa

--- train.sh
+++ train.sh
@@ -5,10 +5,13 @@ rm -rf saving/task1/*
 rm -rf saving/task2/*
 
 echo "start training ..."
-# python train.py -f config/task1/gru.json --random_seed 937
-# python train.py -f config/task1/bigru_atten2.json --random_seed 937
+python train.py -f config/task1/gru.json --random_seed 937 --n_fold 20
+python train.py -f config/task1/bigru_atten2.json --random_seed 937 --n_fold 20
+python train.py -f config/task1/seq2seqatten2.json --random_seed 937 --n_fold 20
 python train.py -f config/task1/rcnn.json --random_seed 937 --n_fold 20
 
-# python train.py -f config/task2/gru.json --random_seed 937
-# python train.py -f config/task2/bigru_atten2.json --random_seed 937
+python train.py -f config/task2/gru.json --random_seed 937 --n_fold 20
+python train.py -f config/task2/bigru_atten2.json --random_seed 937 --n_fold 20
+python train.py -f config/task2/seq2seqatten2.json --random_seed 937 --n_fold 20
 python train.py -f config/task2/rcnn.json --random_seed 937 --n_fold 20
+
