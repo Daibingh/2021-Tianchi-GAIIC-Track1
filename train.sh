74d8361cbf1caf108c78ac08f689f1d81b102030
daibingh <daibingh@gmail.com>
2021-05-08 08:23:07 +0800

    change bert nfold

--- train.sh
+++ train.sh
@@ -13,13 +13,13 @@ python pretrain_bert.py -f config/pretrain/bert_pretrain.json
 
 echo "start training ..."
 
-python train.py -f config/task1/bert.json --random_seed 937 --n_fold 20 --pred_W 5
+python train.py -f config/task1/bert.json --random_seed 937 --n_fold 10 --pred_W 10 
 python train.py -f config/task1/gru.json --random_seed 937 --n_fold 40
 python train.py -f config/task1/bigru_atten2.json --random_seed 904 --n_fold 40
 python train.py -f config/task1/seq2seqatten2.json --random_seed 1039 --n_fold 40
 python train.py -f config/task1/rcnn.json --random_seed 503 --n_fold 40
 
-python train.py -f config/task2/bert.json --random_seed 937 --n_fold 20 --pred_W 5
+python train.py -f config/task2/bert.json --random_seed 937 --n_fold 10 --pred_W 10 
 python train.py -f config/task2/gru.json --random_seed 937 --n_fold 40 
 python train.py -f config/task2/bigru_atten2.json --random_seed 904 --n_fold 40
 python train.py -f config/task2/seq2seqatten2.json --random_seed 1039 --n_fold 40
